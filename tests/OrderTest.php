586dec210d471272a4b63094372782e43e1fca82
Michał Sidorowicz <majkelssss@gmail.com>
2017-07-31 21:32:33 +0200

    order class test

--- tests/OrderTest.php
+++ tests/OrderTest.php
@@ -5,5 +5,42 @@ require_once '../src/classes/Order.php';
 
 class OrderTest extends PHPUnit_Extensions_Database_TestCase
 {
-    
+
+    public function testSaveToDB()
+    {
+        $order               = new Order();
+        $order -> user_id    = "";
+        $order -> product_id = "";
+        $order -> status     = "Nowe";
+        $order -> amount     = "";
+
+        $order -> assertTrue($order -> saveToDB(self::$connection));
+    }
+
+    public function testDeleteFromDB()
+    {
+        $order               = new Order();
+        $order -> user_id    = "";
+        $order -> product_id = "";
+        $order -> status     = "Nowe";
+        $order -> amount     = "";
+
+        $order -> assertTrue($order -> deleteFromDB(self::$connection));
+    }
+
+    public function testloadAllOrders()
+    {
+        
+    }
+
+    public function testloadOrdersByUserId()
+    {
+        
+    }
+
+    public function testloadOrdersById()
+    {
+        
+    }
+
 }
