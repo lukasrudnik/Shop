625f4a657236336986875cd3576ef5a9eda9a606
Michał Sidorowicz <majkelssss@gmail.com>
2017-07-31 21:35:11 +0200

    user page view

--- web/userPage.php
+++ web/userPage.php
@@ -3,62 +3,58 @@ session_start();
 
 require_once '../src/initialClass.php';
 
-if(!isset($_SESSION['userId'])){
+if (!isset($_SESSION['userId'])) {
     header('Location: login.php');
 }
 
 // Aktywna sesja użytkownika
 $userSession = $_SESSION['userId'];
-$loggedUser = User::loadUserById($connect, $userSession);
-
+$loggedUser  = User::loadUserById($connect, $userSession);
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8"> 
+    <head>
+        <meta charset="UTF-8"> 
         <title>User page</title>
         <link rel="stylesheet"
-        href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" 
-        integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-</head>
-<body>
-    <div class="col-md-12">
-        <nav class="navbar navbar-inverse" role="navigation">	
-            <div class="navbar-header"> 
-                <a class="navbar-brand">Hello,
-                    <?php
-                        echo $loggedUser->getName() . " "; 
-                        echo $loggedUser->getSurname(); 
-                    ?> <!-- powitanie zalogowanego użytkownika -->
-                </a>
-                <a class="navbar-brand" href="index.php">Click to run to Shop page</a>
-            </div>
-            <div class="container-fluid">
-                <ul class="nav navbar-nav navbar-right">
-                  <li>
-                    <?php
-                       // przekierowanie na stronę zmiany danych użytkwnika
-                        if(isset($_SESSION['userId'])){              
-                            echo ("<a class=\"dropdown-toggle\" href=\"settings.php\">Settings</a>");
-                        }
-                    ?>
-                    </li>
-                    <li>
-                    <?php
-                        // wylogowanie zalogowanego użytkownika	
-                        if(isset($_SESSION['userId'])){
+              href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" 
+              integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="col-md-12">
+            <nav class="navbar navbar-inverse" role="navigation">	
+                <div class="navbar-header"> 
+                    <a class="navbar-brand">Hello,
+                        <?php
+                        echo $loggedUser -> getName() . " ";
+                        echo $loggedUser -> getSurname();
+                        ?> <!-- powitanie zalogowanego użytkownika -->
+                    </a>
+                    <a class="navbar-brand" href="index.php">Click to run to Shop page</a>
+                </div>
+                <div class="container-fluid">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li>
+                            <?php
+                            // przekierowanie na stronę zmiany danych użytkwnika
+                            if (isset($_SESSION['userId'])) {
+                                echo ("<a class=\"dropdown-toggle\" href=\"settings.php\">Settings</a>");
+                            }
+                            ?>
+                        </li>
+                        <li>
+                            <?php
+                            // wylogowanie zalogowanego użytkownika	
+                            if (isset($_SESSION['userId'])) {
                                 echo ("<a class=\"dropdown-toggle\" href=\"logout.php\">Logout</a>");
-                        }             
-                    ?>
-                   </li>
-                </ul>
+                            }
+                            ?>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <div class="jumbotron">
+                
             </div>
-        </nav>
-        <div class="jumbotron">
-              
-                        
-              
-        </div>
-    </div>
-</body>
+    </body>
 </html>
